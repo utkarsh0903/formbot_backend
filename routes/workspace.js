5e34f0a7481e3b5e931f74060fe229e0ecc19d61
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-12-30 02:31:29 +0530

    feat: shared with user view/edit

--- routes/workspace.js
+++ routes/workspace.js
@@ -7,9 +7,9 @@ const { findOne } = require("../models/user.models");
 router.post("/create-workspace", authMiddleware, async (req, res) => {
   const userId = req.user.id;
   try {
-    const workspace = await Workspace.$wherefindOne({ owner: userId });
+    const workspace = await Workspace.findOne({ owner: userId });
     if (workspace) {
-      return res.status(200).json(isWorkspaceExist);
+      return res.status(200).json(workspace);
     }
     const newWorkspace = await Workspace.create({
       owner: userId,
@@ -25,13 +25,50 @@ router.post("/create-workspace", authMiddleware, async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const id = req.params;
-  const workspace = await Workspace.find({ id });
+  const {id} = req.params;
+  const workspace = await Workspace.findById(id);
   if (!workspace) {
     return res.status(400).json({ message: "No workspace found" });
   }
   res.status(200).json(workspace);
 });
+
+router.put("/sharedWith/:workspaceId", authMiddleware, async (req, res) => {
+    const { workspaceId } = req.params;
+    const { userId, mode } = req.body; 
+  
+    try {
+      const workspace = await Workspace.findById(workspaceId);
+      if (!workspace) {
+        return res.status(404).json({ message: "Workspace not found" });
+      }
+  
+      // Add the user to the specified mode
+      if (mode === "view") {
+        if (!workspace.sharedWith.viewMode.includes(userId)) {
+          workspace.sharedWith.viewMode.push(userId);
+        }
+      } else if (mode === "edit") {
+        if (!workspace.sharedWith.editMode.includes(userId)) {
+          workspace.sharedWith.editMode.push(userId);
+        }
+      } else {
+        return res.status(400).json({ message: "User can access already" });
+      }
+  
+      await workspace.save();
+  
+      return res.status(200).json({
+        message: `User added to ${mode} mode successfully`,
+        workspace,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "An error occurred while adding the user",
+        error: error.message,
+      });
+    }
+  });  
 
 // router.post("/create-folder", (req, res) => {
 //     const {folderName} = req.body;
